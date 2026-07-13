e39859b8865d9f6cec49fd22383df83595998475
Holger2005 <ordonezholger43@gmail.com>
2026-07-13 11:47:42 -0500

    creación de la función calcularDisponible, función calcular y funcionamiento del botón Calcular Crédito

--- funciones.js
+++ funciones.js
@@ -1,1 +1,12 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos, egresos) {
+
+    let calcular = ingresos - egresos;
+    let redondiarCalcular = calcular.toFixed(2);
+    let calcularRedondeado = parseFloat(calcular.toFixed(2));
+    if (calcularRedondeado < 0) {
+        return calcularRedondeado = 0;
+    } else if(calcularRedondeado >= 0){
+        return calcularRedondeado;
+    }
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,15 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular() {
+    let cmpIngreso = document.getElementById("txtIngresos");
+    let cmpEgreso = document.getElementById("txtEgresos");
+
+    let ingresoStr = cmpIngreso.value;
+    let egresoStr = cmpEgreso.value;
+
+    let calcular = calcularDisponible(ingresoStr, egresoStr);
+
+    let cmpspnDisponible = document.getElementById("spnDisponible");
+
+    cmpspnDisponible.textContent = calcular;
+
+}
